7259390c4439be06b0c6b6ef5339e8693fa936d7
Farkas Pongrácz <pongraczfarkas5@gmail.com>
2022-12-20 13:40:00 +0200

    pre-exam fixed

--- pre-exam.sh
+++ pre-exam.sh
@@ -10,7 +10,11 @@ ansible-playbook infra.yaml --diff
 
 ansible-playbook infra.yaml --diff
 
-ansible all -b -m reboot -a "test_command=uptime"
+ansible pongraczfarkas-1 -b -m reboot -a "test_command=uptime"
+ansible pongraczfarkas-2 -b -m reboot -a "test_command=uptime"
+ansible pongraczfarkas-3 -b -m reboot -a "test_command=uptime"
+
+sleep 30
 
 ansible-playbook infra.yaml --diff
 
